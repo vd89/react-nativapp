946d55512f4217902cace3fe378de11aea85366a
vd89 <dixitvora@gmail.com>
2020-10-14 14:37:54 +0530

    Chore: Set up and use of StyleSheet

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const { root, input, inputContainer } = styles;
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={root}>
+      <View style={inputContainer}>
+        <TextInput placeholder='Enter Course Goal' style={input} />
+        <Button title='Add' />
+      </View>
+      <View></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  root: { padding: 35 },
+  inputContainer: { flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' },
+  input: { borderBottomColor: 'black', width: '80%', borderBottomWidth: 1, margin: 2, padding: 10 },
 });
