7e61c89cd58780e8dd7e523cdb1414398aa139bb
vd89 <dixitvora@gmail.com>
2020-10-14 15:56:44 +0530

    Chore: Completed the text input component for react native

--- App.js
+++ App.js
@@ -1,22 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
-  const { root, input, inputContainer } = styles;
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const onchangeHandler = async (enteredText) => {
+    await setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals((courseGoals) => [...courseGoals, enteredGoal]);
+  };
+  const { root, input, inputContainer, listItem } = styles;
   return (
     <View style={root}>
       <View style={inputContainer}>
-        <TextInput placeholder='Enter Course Goal' style={input} />
-        <Button title='Add' />
+        <TextInput
+          placeholder='Enter Course Goal'
+          style={input}
+          onChangeText={(text) => onchangeHandler(text)}
+          value={enteredGoal}
+        />
+        <Button title='Add' onPress={addGoalHandler} />
       </View>
-      <View></View>
+      <View>
+        {courseGoals.map((goal) => (
+          <View style={listItem} key={goal}>
+            <Text> ❤️ {goal} </Text>
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  root: { padding: 35 },
-  inputContainer: { flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' },
-  input: { borderBottomColor: 'black', width: '80%', borderBottomWidth: 1, margin: 2, padding: 10 },
+  root: {
+    padding: 35,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  input: {
+    borderBottomColor: 'black',
+    width: '80%',
+    borderBottomWidth: 1,
+    margin: 2,
+    padding: 10,
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1,
+  },
 });
